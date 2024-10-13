cbeb79ab867bc128e9143d4dd0d4d0f680214ef2
JaysonY <36421979+jaykingboss@users.noreply.github.com>
2024-10-13 16:26:38 -0700

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,179 @@
+#include <iostream>
+using namespace std;
+
+const int MIN_NR = 10, MAX_NR = 99, MIN_LS = 5, MAX_LS = 20;
+
+class DoublyLinkedList {
+private:
+    struct Node {
+        int data;
+        Node* prev;
+        Node* next;
+        Node(int val, Node* p = nullptr, Node* n = nullptr) {
+            data = val;
+            prev = p;
+            next = n;
+        }
+    };
+    Node* head;
+    Node* tail;
+
+public:
+    DoublyLinkedList() : head(nullptr), tail(nullptr) {}
+
+    void push_back(int value) {
+        Node* newNode = new Node(value);
+        if (!tail) { 
+            head = tail = newNode;
+        } else {
+            tail->next = newNode;
+            newNode->prev = tail;
+            tail = newNode;
+        }
+    }
+
+    void push_front(int value) {
+        Node* newNode = new Node(value);
+        if (!head) { 
+            head = tail = newNode;
+        } else {
+            newNode->next = head;
+            head->prev = newNode;
+            head = newNode;
+        }
+    }
+
+    void delete_pos(int position) {
+        if (position < 0) {
+            cout << "Position must be >= 0." << endl;
+            return;
+        }
+        Node* temp = head;
+        for (int i = 0; i < position && temp; ++i) {
+            temp = temp->next;
+        }
+        if (!temp) {
+            cout << "Position exceeds list size. Node not deleted.\n";
+            return;
+        }
+        if (temp->prev) {
+            temp->prev->next = temp->next;
+        } else {
+            head = temp->next; 
+        }
+        if (temp->next) {
+            temp->next->prev = temp->prev;
+        } else {
+            tail = temp->prev; 
+        }
+        delete temp;
+    }
+
+    void pop_front() {
+        if (!head) return; 
+        Node* temp = head;
+        head = head->next;
+        if (head) {
+            head->prev = nullptr;
+        } else {
+            tail = nullptr;
+        }
+        delete temp;
+    }
+
+    void pop_back() {
+        if (!tail) return; 
+        Node* temp = tail;
+        tail = tail->prev;
+        if (tail) {
+            tail->next = nullptr;
+        } else {
+            head = nullptr;
+        }
+        delete temp;
+    }
+
+    void delete_val(int value) {
+        if (!head) return; 
+        Node* temp = head;
+        while (temp && temp->data != value) {
+            temp = temp->next;
+        }
+        if (!temp) return; 
+        if (temp->prev) {
+            temp->prev->next = temp->next;
+        } else {
+            head = temp->next;
+        }
+        if (temp->next) {
+            temp->next->prev = temp->prev;
+        } else {
+            tail = temp->prev; 
+        }
+        delete temp;
+    }
+
+    void print() {
+        Node* current = head;
+        if (!current) {
+            cout << "List is empty" << endl;
+            return;
+        }
+        while (current) {
+            cout << current->data << " ";
+            current = current->next;
+        }
+        cout << endl;
+    }
+
+    void print_reverse() {
+        Node* current = tail;
+        if (!current) {
+            cout << "List is empty" << endl;
+            return;
+        }
+        while (current) {
+            cout << current->data << " ";
+            current = current->prev;
+        }
+        cout << endl;
+    }
+
+    ~DoublyLinkedList() {
+        while (head) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+};
+
+int main() {
+    DoublyLinkedList list;
+
+    for (int i = 0; i < 10; ++i) {
+        list.push_back(rand() % (MAX_NR - MIN_NR + 1) + MIN_NR);
+    }
+
+    cout << "List after adding elements:" << endl;
+    list.print();
+
+    cout << "Deleting position 3:" << endl;
+    list.delete_pos(3);
+    list.print();
+
+    cout << "Popping front:" << endl;
+    list.pop_front();
+    list.print();
+
+    cout << "Popping back:" << endl;
+    list.pop_back();
+    list.print();
+
+    cout << "Deleting value 5 (if exists):" << endl;
+    list.delete_val(5);
+    list.print();
+
+    return 0;
+}
+
